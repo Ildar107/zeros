b7cb8c4deb01158543012956b8278355c1eb2b3a
ildar107 <ildar107wk@gmail.com>
2019-10-03 17:34:08 +0300

    fix: change decision

--- src/index.js
+++ src/index.js
@@ -1,33 +1,58 @@
 function factorial (num, isOdd){
-  var result = BigInt(1);
+  var fiveCounter = 0;
+  var twoCounter = 0;
   for(var i = 1; i <= num; i++)
   {
-      if( isOdd === null)
-        result *= BigInt(i);
-      if(isOdd !== null && isOdd && i % 2 > 0)
-        result *= BigInt(i);
-      if(isOdd !== null && !isOdd && i % 2 === 0)
-        result *= BigInt(i);   
+      if(i % 5 === 0 && i % 10 !== 0 && (isOdd === null || isOdd))
+        fiveCounter += i/5;
+      if(i % 2 === 0 && i % 10 !== 0)
+        twoCounter += i/2;  
+      if(i % 10 === 0)
+      {
+        fiveCounter ++;
+        fiveCounter += i/10 === 5 ? 1 : 0;
+        twoCounter++;
+      } 
   }
-  return result;
+    return {
+      fiveCounter: fiveCounter,
+      twoCounter: twoCounter
+    };
+  
 }
 
 module.exports = function zeros(expression) {
   var itemsOfExpression = expression.split("*");
   var resultOfExpression = 1n;
   var result = 0;
+  var fiveCounter = 0;
+  var zeroCounter = 0;
+  var twoCounter = 0;
   itemsOfExpression.forEach(element => {
       var num = Number(element.split("!")[0]);
       if(element.includes("!!"))
-        resultOfExpression *= factorial(num, num % 2 > 0);
+      {
+        if(num % 2 > 0)
+        {
+            fiveCounter += factorial(num, true).fiveCounter;
+        }
+        else
+        {
+          let temp =  factorial(num, false);
+          fiveCounter += temp.fiveCounter;
+          twoCounter += temp.twoCounter;
+        }
+      }
       else
-        resultOfExpression *= factorial(num, null);
+      {
+        let temp =  factorial(num, null);
+        fiveCounter += temp.fiveCounter;
+        twoCounter += temp.twoCounter;
+      }
   });
-  while(resultOfExpression % 10n === 0n)
-  {
-      result++;
-      resultOfExpression = resultOfExpression / 10n;
-  }
-  return result;
+  console.log( fiveCounter + " " + twoCounter )
+    zeroCounter = Math.min(fiveCounter, twoCounter);
+
+  return zeroCounter;
 }
 
